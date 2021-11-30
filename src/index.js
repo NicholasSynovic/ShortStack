6cf3f81d4657ccc290719e1cff53413031c3d65d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-11-29 21:11:14 -0600

    Complete Getting Started with Firebase 9 #3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWLtpyFyzKHGliA5I57DzIEJDdPxna9M8",
+    authDomain: "shortstack-messaging.firebaseapp.com",
+    projectId: "shortstack-messaging",
+    storageBucket: "shortstack-messaging.appspot.com",
+    messagingSenderId: "763774487413",
+    appId: "1:763774487413:web:ac1cdc075ede980c99213c",
+    measurementId: "G-YSCM3NLK9Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
