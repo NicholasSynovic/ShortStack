6e90d3a24f0674c6faa7b717dafec8493aa1714c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-11-30 10:38:49 -0600

    Implement code to sign users up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, onSnapshot, query, where } from "firebase/firestore"
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+// import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, onSnapshot, query, where } from "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: "AIzaSyAWLtpyFyzKHGliA5I57DzIEJDdPxna9M8",
@@ -13,16 +13,55 @@ const firebaseConfig = {
     measurementId: "G-YSCM3NLK9Q"
 }
 
-// Initialize Firebase
+// Initialize Firebase Components
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initalize Firestore
-const firestore = getFirestore()
 const authentication = getAuth()
 
-// Reference collection in Firestore
-const collectionReference = collection(firestore, "books")
+// Firebase Authentication
+
+// Sign Up
+const signUpForm = document.getElementById("signUp-form")
+signUpForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const email = signUpForm.email.value
+    const password = signUpForm.password.value
+
+    createUserWithEmailAndPassword(authentication, email, password)
+        .then((credential) => {
+            signUpForm.reset()
+            location.href = "app/index.html"
+            // console.log("user created", credential.user)
+        })
+        .catch(err => {
+            alert(err.message)
+        })
+    authetnicationForm.reset()
+})
+
+// Sign in
+const signInForm = document.getElementById("signIn-form")
+signInForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const email = signInForm.email.value
+    const password = signInForm.password.value
+
+    createUserWithEmailAndPassword(authentication, email, password)
+        .then((credential) => {
+            signInForm.reset()
+            location.href = "app/index.html"
+            // console.log("user created", credential.user)
+        })
+        .catch(err => {
+            alert(err.message)
+        })
+    authetnicationForm.reset()
+})
+
+// const firestore = getFirestore()
+// const collectionReference = collection(firestore, "books")
 
 // Print out all objects in the collection REQUIRES REFRESH TO VIEW NEW DATA
 // getDocs(collectionReference).then((snapshot) => {
@@ -36,55 +75,36 @@ const collectionReference = collection(firestore, "books")
 //         console.log(err.message)
 // })
 
-// Print out all objects in the collection IN REAL TIME
-onSnapshot(collectionReference, (snapshot) => {
-    let books = []
-    snapshot.docs.forEach((doc) => {
-        books.push({ ...doc.data(), id: doc.id })
-    })
-    console.log(books)
-})
+// // Print out all objects in the collection IN REAL TIME
+// onSnapshot(collectionReference, (snapshot) => {
+//     let books = []
+//     snapshot.docs.forEach((doc) => {
+//         books.push({ ...doc.data(), id: doc.id })
+//     })
+//     console.log(books)
+// })
 
-// Add book to FireStore Books collection
-const addBookForm = document.getElementById("addData")
-addBookForm.addEventListener("submit", (e) => {
-    e.preventDefault()
+// // Add book to FireStore Books collection
+// const addBookForm = document.getElementById("addData")
+// addBookForm.addEventListener("submit", (e) => {
+//     e.preventDefault()
 
-    addDoc(collectionReference, {
-        title: addBookForm.title.value,
-        author: addBookForm.author.value,
-    }).then(() => {
-        addBookForm.reset()
-    })
-})
+//     addDoc(collectionReference, {
+//         title: addBookForm.title.value,
+//         author: addBookForm.author.value,
+//     }).then(() => {
+//         addBookForm.reset()
+//     })
+// })
 
-// Delete book from FireStore Books collection
-const deleteBookForm = document.getElementById("deleteData")
-deleteBookForm.addEventListener("submit", (e) => {
-    e.preventDefault()
+// // Delete book from FireStore Books collection
+// const deleteBookForm = document.getElementById("deleteData")
+// deleteBookForm.addEventListener("submit", (e) => {
+//     e.preventDefault()
 
-    const documentReference = doc(firestore, "books", deleteBookForm.id.value)
+//     const documentReference = doc(firestore, "books", deleteBookForm.id.value)
 
-    deleteDoc(documentReference).then(() => {
-        deleteBookForm.reset()
-    })
-})
-
-// Firebase Authentication
-// Create User
-const authetnicationForm = document.getElementById("authentication")
-authetnicationForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    const email = authetnicationForm.email.value
-    const password = authetnicationForm.password.value
-
-    createUserWithEmailAndPassword(authentication, email, password)
-        .then((credential) => {
-            console.log("user created", credential.user)
-        })
-        .catch(err => {
-            console.log(err.message)
-        })
-    authetnicationForm.reset()
-})
+//     deleteDoc(documentReference).then(() => {
+//         deleteBookForm.reset()
+//     })
+// })
