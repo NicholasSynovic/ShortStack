e3c4e19461ea9ae803338c943f79fc945e65e89e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-11-29 21:19:31 -0600

    Complete Getting Started with Firebase 9 #4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getFirestore, collection, getDocs } from "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: "AIzaSyAWLtpyFyzKHGliA5I57DzIEJDdPxna9M8",
@@ -9,8 +10,25 @@ const firebaseConfig = {
     messagingSenderId: "763774487413",
     appId: "1:763774487413:web:ac1cdc075ede980c99213c",
     measurementId: "G-YSCM3NLK9Q"
-};
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initalize Firestore
+const firestore = getFirestore()
+
+// Reference collection in Firestore
+const collectionReference = collection(firestore, "books")
+
+getDocs(collectionReference).then((snapshot) => {
+    let books = []
+    snapshot.docs.forEach((doc) => {
+        books.push({...doc.data(), id: doc.id})
+    })
+    console.log(books)
+})
+    .catch(err => {
+        console.log(err.message)
+})
