a3212280b08615299a8f938259ac27146c7077c5
NicholasSynovic <NicholasSynovic@users.noreply.github.com>
2021-12-02 07:04:37 +0000

    deploy: 29f96e957815c0df1683c9cb14ce3e423eb32b06

---
+++ components/about.js
@@ -0,0 +1,19 @@
+function about() {
+    const aboutContainer = document.getElementById("about")
+    const template = document.createElement("template")
+
+    template.innerHTML = `
+        <div id="icon-container">
+            <img src="./img/layers.png" class="icon">
+        </div>
+        <div id="app-info">
+            <h1>ShortStack Messaging</h1>
+            <p>Proof of concept chat PWA</p>
+            <p>Application made by Nicholas Synovic</p>
+            <p>Icons made by <a href="https://www.flaticon.com/authors/bukeicon"
+                title="bukeicon">bukeicon</a> from <a href="https://www.flaticon.com/"
+                title="Flaticon">www.flaticon.com</a></p>
+        </div>
+    `
+    aboutContainer.appendChild(template.content)
+}
---
+++ components/navbar.js
@@ -0,0 +1,70 @@
+function navbarHome() {
+    const navbarContainer = document.getElementById("nav-bar")
+    const template = document.createElement("template")
+
+    template.innerHTML = `
+        <div class="nav-item" onclick="location.href='index.html'">
+            <h3>ShortStack Messaging</h3>
+        </div>
+        <div class="nav-item">
+            <button id="signIn-button" class="nav-button" onclick="location.href='signIn.html'">Sign In</button>
+        </div>
+        <div class="nav-item">
+            <button id="signUp-button" class="nav-button" onclick="location.href='signUp.html'">Sign Up</button>
+        </div>
+    `
+    navbarContainer.appendChild(template.content)
+}
+
+function navbarSignIn() {
+    const navbarContainer = document.getElementById("nav-bar")
+    const template = document.createElement("template")
+
+    template.innerHTML = `
+        <div class="nav-item" onclick="location.href='index.html'">
+            <h3>ShortStack Messaging</h3>
+        </div>
+        <div></div>
+        <div class="nav-item">
+            <button id="signUp-button" class="nav-button" onclick="location.href='signUp.html'">Sign Up</button>
+        </div>
+    `
+    navbarContainer.appendChild(template.content)
+}
+
+function navbarSignUp() {
+    const navbarContainer = document.getElementById("nav-bar")
+    const template = document.createElement("template")
+
+    template.innerHTML = `
+        <div class="nav-item" onclick="location.href='index.html'">
+            <h3>ShortStack Messaging</h3>
+        </div>
+        <div></div>
+        <div class="nav-item">
+            <button id="signIn-button" class="nav-button" onclick="location.href='signIn.html'">Sign In</button>
+        </div>
+    `
+    navbarContainer.appendChild(template.content)
+}
+
+function navbarApp() {
+    const navbarContainer = document.getElementById("nav-bar")
+    const template = document.createElement("template")
+
+    template.innerHTML = `
+        <div class="nav-item" onclick="location.href='../index.html'">
+            <h3>ShortStack Messaging</h3>
+        </div>
+        <div class="nav-item-lesser-round">
+            <section id="profile">
+                <img id="profile-icon" alt="">
+                <h3 id="displayName-greeting">{User}</h3>
+            </section>
+        </div>
+        <div class="nav-item">
+            <button id="logout-button" class="nav-button">Sign Out</button>
+        </div>
+    `
+    navbarContainer.appendChild(template.content)
+}
