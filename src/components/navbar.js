85a12e0c3a93af3762a22a94b5ff7f53c7a4588c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-12-02 00:43:49 -0600

    Add navbar component to app

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -47,3 +47,24 @@ function navbarSignUp() {
     `
     navbarContainer.appendChild(template.content)
 }
+
+function navbarApp() {
+    const navbarContainer = document.getElementById("nav-bar")
+    const template = document.createElement("template")
+
+    template.innerHTML = `
+        <div class="nav-item" onclick="location.href='../index.html'">
+            <h3>ShortStack Messaging</h3>
+        </div>
+        <div class="nav-item">
+            <section id="profile">
+                <img id="profile-icon" alt="">
+                <h3 id="displayName-greeting">Hello, {User}</h3>
+            </section>
+        </div>
+        <div class="nav-item">
+            <button id="logout-button" class="button">Sign Out</button>
+        </div>
+    `
+    navbarContainer.appendChild(template.content)
+}
