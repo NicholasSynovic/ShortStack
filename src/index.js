4c7fed39c3ceee84515e7fc60ba3501afc0d731e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-12-07 16:28:19 -0600

    Remove highlight.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ marked.setOptions({
         const language = hljs.getLanguage(lang) ? lang : 'plaintext';
         return hljs.highlight(code, { language }).value;
     },
-    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
     pedantic: false,
     gfm: true,
     breaks: false,
